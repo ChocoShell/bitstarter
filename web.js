4f1d1c4aa500505aa0a54306a441a07f275edede
Ubuntu <ubuntu@ip-172-31-38-15.us-west-2.compute.internal>
2013-07-11 06:22:13 +0000

    asdfg

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ app.get('/', function(request, response) {
   var new_msg = fs.readFileSync('index.html', 'utf8');
   var buffer = new Buffer(new_msg, "utf-8");
   response.send(buffer.toString);
-};
+});
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
