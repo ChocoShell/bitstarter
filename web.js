eeed03a822e06eac198a673af5c37a5316fec772
Ubuntu <ubuntu@ip-172-31-21-15.us-west-2.compute.internal>
2013-08-02 16:21:39 +0000

    bootstrapped

--- web.js
+++ web.js
@@ -1,15 +1,13 @@
 var express = require('express');
 var fs = require('fs');
-var buf = require('buffer');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var new_msg = fs.readFileSync('index.html', 'utf8');
-  var buffer = new Buffer(new_msg, "utf-8");
-  response.send(buffer.toString('utf-8'));
+  var new_msg = fs.readFileSync('index.html').toString();
+  response.send(new_msg);
 });
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
